d4d0ece416db394bc6b5ed3b199e781ae2575e31
Ben Chasin <ben@pop-os.localdomain>
2020-09-11 14:50:50 -0700

    deployment proxy

--- server/app.js
+++ server/app.js
@@ -1,8 +1,16 @@
 const path = require('path');
 const express = require('express');
+require('newrelic');
+const { createProxyMiddleware } = require('http-proxy-middleware');
 
 const app = express();
 
-app.use(express.static(path.join(__dirname, '/../public')));
+app.use('/:gameID', express.static(path.join(__dirname, '../public')));;
+
+app.use('/api/getGameByID/:gameID', createProxyMiddleware({ target: 'http://localhost:3003' }));
+
+app.use('/api/getGamesByTags/:gameID/:tag', createProxyMiddleware({ target: 'http://localhost:3003' }));
+
+app.use('/api/getGamesBySeries/:gameID/:series', createProxyMiddleware({ target: 'http://localhost:3003' }));
 
 module.exports.app = app;
